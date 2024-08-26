35e1fe1d39ffd6380e2a6de34b8f4928369cf58c
Almat Kairatov <92521753+almatkai@users.noreply.github.com>
2024-08-27 00:39:41 +0500

    Initial Commit

---
+++ EffectiveMobile_Todo/EffectiveMobile_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobile_TodoApp.swift
+//  EffectiveMobile_Todo
+//
+//  Created by Almat Kairatov on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobile_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
